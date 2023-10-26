aa9e46c3402c17429e8e8cb6cfbbade1ebb169bc
Sarthak <sarthakbhatt@live.com>
2023-10-27 01:47:10 +0530

    Added accordian and nextui

--- src/app/Components/Chat.tsx
+++ src/app/Components/Chat.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { Button, ButtonGroup } from "@nextui-org/react";
+import React from "react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 
-export default function Home() {
+export default function Chat() {
   const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
   return (
-    <main className="absolute inset-0 flex justify-center items-center text-4xl flex-col ">
-      Website content
-      <Button className="duration-300" color="primary">
-        Button
-      </Button>
-    </main>
+    <div className="max-w-[560px] min-h-[500px]">
+      <Accordion>
+        <AccordionItem aria-label="Accordion 1" title="Accordion 1">
+          {defaultContent}
+        </AccordionItem>
+      </Accordion>
+    </div>
   );
 }
